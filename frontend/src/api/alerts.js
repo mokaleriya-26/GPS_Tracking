82c712ff1d2b69bad173c152ba98f0db53a27617
snehalbytes <snehal.mascarenhas.21@gmail.com>
2026-09-23 22:08:58 +0530

    Update frontend API configuration

--- frontend/src/api/alerts.js
+++ frontend/src/api/alerts.js
@@ -1,6 +1,6 @@
-import api from './axios';
+import api from './axios.js';
 
 export const getAlerts = async () => {
-  const response = await api.get('/alerts');
+  const response = await api.get('/api/alerts/');
   return response.data;
 };
--- frontend/src/api/vehicles.js
+++ frontend/src/api/vehicles.js
@@ -1,6 +1,6 @@
-import api from './axios';
+import api from './axios.js';
 
 export const getVehicles = async () => {
-  const response = await api.get('/data');
+  const response = await api.get('/api/alerts/');
   return response.data;
 };
